82af37e35d7a38d1fec7aed23ab8a70acc78bb5e
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-09-03 17:05:21 +0200

    Add UserInfo task

---
+++ src/main/java/com/softserve/academy/homework1/UserInfo.java
@@ -0,0 +1,18 @@
+package com.softserve.academy.homework1;
+
+import java.util.Scanner;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        String name, address;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your name?: ");
+        name = input.nextLine();
+
+        System.out.print("Where do you live, " + name + "?");
+        address = input.nextLine();
+        
+        System.out.println("Name: " + name +  ", Address: " + address);
+    }
+}
