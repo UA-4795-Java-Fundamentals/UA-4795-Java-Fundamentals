d4f5d90da0b9770e07624ce97db96683b8fe6903
AntoninaNovytska <ann280785@gmail.com>
2025-10-24 10:05:01 +0300

    Module3.Pr.task1

---
+++ src/main/java/com/softserve/academy/module3/PracticalTask03.java
@@ -0,0 +1,31 @@
+package main.java.com.softserve.academy.module3;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class PracticalTask03 {
+    public static void main(String[] args) {
+        int first = getNumber("Enter the first number: ");
+        int second = getNumber("Enter the second number: ");
+        int sum = getTotal(first, second);
+        double average = getAverage(first, second);
+        System.out.println("The sum of " + first + " and " + second + " is " + sum);
+        System.out.println("The average of " + first + " and " + second + " is " + average);
+    }
+
+    private static int getNumber(String prompt) {
+        System.out.print(prompt);
+        Scanner scan = new Scanner(in);
+        return Integer.parseInt(scan.nextLine());
+    }
+
+    private static double getAverage(int first, int second) {
+        return getTotal(first, second) / 2.0;
+    }
+
+    private static int getTotal(int first, int second) {
+        return first + second;
+        
+    }
+}
